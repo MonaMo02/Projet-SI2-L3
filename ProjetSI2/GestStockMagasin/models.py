241644eb84f7129c5270bcb67ce29ae676498eaa
MonaMo02 <djeraouimouna@gmail.com>
2022-12-31 16:14:28 +0100

    Mouna Commit

--- ProjetSI2/GestStockMagasin/models.py
+++ ProjetSI2/GestStockMagasin/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class TypeProduit (models.Model):
+    CodeT = models.CharField(primary_key=True ,max_length=10)
+    DesignationT = models.CharField(max_length=100)
+
+
+class Produit (models.Model):
+    CodeP = models.CharField(primary_key=True, max_length=10)
+    DesignationP = models.CharField(max_length=100)
+    Type = models.ForeignKey(TypeProduit, on_delete=models.CASCADE)
+    Fournisseur = models.ForeignKey(Fournisseur, on_delete=models.CASCADE)
+
+
+class Client (models.Model):
+    CodeC = models.CharField(primary_key= True, max_length=10)
+    NomC = models.CharField
+    PrenomC = models.CharField
+    AdresseC = models.CharField
+    TelephoneC = models.CharField
+    Acheter = models.ManyToManyField(Produit, related_name="Client")
+
+class Fournisseur(models.Model):
+    CodeF = models.CharField(primary_key=True, max_length=10)
+    NomF = models.CharField
+    PrenomF = models.CharField
+    AdresseF = models.CharField
+    TelephoneF = models.CharField
