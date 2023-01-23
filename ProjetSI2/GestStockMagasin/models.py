4a4c484d854eeacf1fa2bdd86b26e5ce365d7bb5
MonaMo02 <djeraouimouna@gmail.com>
2023-01-23 20:01:06 +0100

    Mouna Commit

--- ProjetSI2/GestStockMagasin/models.py
+++ ProjetSI2/GestStockMagasin/models.py
@@ -24,6 +24,7 @@ class Produit (models.Model):
     CodeP = models.AutoField(primary_key=True)
     DesignationP = models.CharField(max_length=100)
     Type = models.ForeignKey(TypeProduit, on_delete=models.CASCADE)
+    Fournisseur = models.ForeignKey(Fournisseur, on_delete=models.CASCADE)
     def __str__(self):         
         return str(self.DesignationP)
 
